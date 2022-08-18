de3b4134c1cab59c1f15345ca832666ef545e2d2
atomotavio <atomotavio@gmail.com>
2022-08-18 20:35:46 -0300

    feature: make components

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Navbar from "./components/navbar";
+
+function App() {
+  return (
+    <Navbar/>
+  );
+}
+
+export default App;
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,14 @@
+function Navbar () {
+    return (
+        <>
+        <a href="#">Home</a>
+        <a href="#">Quem sou eu</a>
+        <a href="#">Experiencia profissional</a>
+        <a href="#">Formação</a>
+        <a href="#">Cursos e formações extras</a>
+        <a href="#">Projetos</a>
+        </>
+    )
+}
+
+export default Navbar;
